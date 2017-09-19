16bab5506de7796708893eeed98b5e5707e4a586
luzhiqiang <listener_lzq@163.com>
2017-09-20 00:19:16 +0800

    add component Checkbox

---
+++ src/components/common/checkbox/CheckboxGroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="she-checkbox-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { ComponentPrefix } from '@config'
+export default {
+  name: `${ComponentPrefix}-checkbox-group`,
+  componentName: 'checkbox-group',
+  props: {
+    value: Array
+  },
+  computed: {
+    localValue: {
+      get: function () {
+        return this.value
+      },
+      set: function(newVal) {
+        this.$emit('input', newVal)
+      }
+    }
+  }
+}
+</script>
+
